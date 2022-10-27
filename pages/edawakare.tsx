1afe2634d9e55ecd4dfa9befd21ddbcaa5e0147a
JasonK <jason@karjadi.com>
2022-10-27 16:46:17 +0900

    reset edawakare page

--- pages/edawakare.tsx
+++ pages/edawakare.tsx
@@ -2,43 +2,32 @@ import Head from "next/head";
 import { FC } from "react";
 
 const EdaWakare: FC = () => {
-  const hiraganaRow =
-    "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをん";
-  const katakanaRow =
-    "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲン";
-  // may need to seperate kanarow rowbtn to be FC // 赤　金　目　心　手　見　女　戸　事言　日火　神上　所　子　人　一　二　取　切　固　水　木
   return (
     <>
       <Head>
         <title>「コトミ」枝分かれ</title>
       </Head>
-      <>
-        <div className="kanarow">
-          {(true ? hiraganaRow : katakanaRow).split("").map((val, idx) => (
-            <button key={idx} className="rowbtn">
-              {val}
-            </button>
-          ))}
-        </div>
-      </>
       <style jsx>{`
         .kanarow {
           display: flex;
           flex-direction: row-reverse;
           justify-content: space-between;
           position: absolute;
-          bottom: 10px;
+          top: 50%;
           left: 0;
-          padding: 0 10px;
+          transform: translateY(-50%);
+          padding: 0.5rem;
         }
         .rowbtn {
           border: none;
           background: rgba(255, 255, 255, 0.2);
-          border-radius: 5px;
+          border-radius: 0.5rem;
           flex: 1;
+          height: 2rem;
+          width: 2rem;
         }
         .rowbtn + .rowbtn {
-          margin-right: 5px;
+          margin-right: 0.5rem;
         }
         .rowbtn:hover {
           background: rgba(255, 255, 255, 0.6);
